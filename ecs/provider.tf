510bb9d3e748b2272d707d4ec886b73f3245e5e0
srd785 <107203103+srd785@users.noreply.github.com>
2023-02-21 11:26:09 +0530

    Update provider.tf

--- ecs/provider.tf
+++ ecs/provider.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  #access_key = "add-your-key"
-  #secret_key = "add-your-access_key"
+  access_key = "AKIATG7BYF2NBKA6ZLP6"
+  secret_key = "aKSoeAlXWgaQ7x241VTG3UiZEQl3WD5sxabkRAme"
   region     = var.aws_region
   #if you are running from AWS ec2 linux instance please use bellow credentials section
   #shared_credentials_file = "$HOME/.aws/credentials"
